a87365114b849290eded73a6fe5ab33b1de07d5d
Daniel <Daniel.falklimberg@gmail.com>
2021-01-26 10:43:42 +0100

    initial

---
+++ src/main/java/wisemen/movienights/MovienightsApplication.java
@@ -0,0 +1,13 @@
+package wisemen.movienights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovienightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovienightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wisemen/movienights/MovienightsApplicationTests.java
@@ -0,0 +1,13 @@
+package wisemen.movienights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovienightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
