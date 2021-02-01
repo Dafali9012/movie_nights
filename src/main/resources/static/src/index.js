394238bdb4c57c142818d4fc9a2d2b93a48c883d
David Glane <david@glane.net>
2021-02-01 12:29:50 +0100

    fetch, danneboi was here

---
+++ src/main/resources/static/src/index.js
@@ -0,0 +1,17 @@
+
+
+fetchMedia().then((a) => {
+    console.log(a)
+    document.getElementById("mov").innerHTML=`
+    <h2>
+    ${
+        a.map(a => {
+            return a.Title
+        })
+    }
+    </h2>
+`});
+
+async function fetchMedia(){
+    return await (await fetch("http://localhost:5500/rest/v1/media?title=deadpool")).json();
+}
