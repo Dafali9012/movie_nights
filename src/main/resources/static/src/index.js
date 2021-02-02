5c682c0c6e6f226f33101e318821acda3bde6e30
David Glane <david@glane.net>
2021-02-02 20:07:32 +0100

    search fetch, refactor

--- src/main/resources/static/src/index.js
+++ src/main/resources/static/src/index.js
@@ -1,9 +1,18 @@
-fetchMedia().then(a => {
-    a.map(a=>{
-        document.getElementById("mov").innerHTML = document.getElementById("mov").innerHTML.concat(`<img class="poster" src=${a.Poster} />`); 
-    });
+document.getElementById("focus").focus();
+document.getElementById("btn-Submit").addEventListener('click', function() {
+    document.getElementById("mov").innerHTML = "";
+    fetchMedia().then(a => {
+        a.map(a=>{
+            document.getElementById("mov").innerHTML = document.getElementById("mov").innerHTML.concat(`
+            <img class="poster" src=${a.Poster} alt="poster"/>`);
+        });
+    })
 });
 
+
+
+
 async function fetchMedia(){
-    return await (await fetch("http://localhost:5500/rest/v1/media?title=super mario")).json();
+    console.log("searching for " + document.getElementById("searchInput").value);
+    return await (await fetch('http://localhost:5500/rest/v1/media?title=' + document.getElementById("searchInput").value)).json();
 }
