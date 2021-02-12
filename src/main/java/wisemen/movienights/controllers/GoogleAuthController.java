9d124b1743201cb81323db433774ffeb2e64b909
razvan22 <razviy69@gmail.com>
2021-02-12 13:31:07 +0100

    add-event-frontend

--- src/main/java/wisemen/movienights/controllers/GoogleAuthController.java
+++ src/main/java/wisemen/movienights/controllers/GoogleAuthController.java
@@ -76,128 +76,64 @@ public class GoogleAuthController {
         }
 
         GoogleIdToken.Payload payload = idToken.getPayload();
-
-
-        // Use THIS ID as a key to identify a google user-account.
-
         String userId = payload.getSubject();
-
-
         String email = payload.getEmail();
-
         boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-
         String name = (String) payload.get("name");
-
         String pictureUrl = (String) payload.get("picture");
-
         String locale = (String) payload.get("locale");
-
         String familyName = (String) payload.get("family_name");
-
         String givenName = (String) payload.get("given_name");
-
-
         // Debugging purposes, should probably be stored in the database instead (At least "givenName").
-
         System.out.println("userId: " + userId);
-
         System.out.println("email: " + email);
-
         System.out.println("emailVerified: " + emailVerified);
-
         System.out.println("name: " + name);
-
         System.out.println("pictureUrl: " + pictureUrl);
-
         System.out.println("locale: " + locale);
-
         System.out.println("familyName: " + familyName);
-
         System.out.println("givenName: " + givenName);
 
-
         GoogleCredential credentials = new GoogleCredential().setAccessToken(accessToken);
-
         Calendar calendar = new Calendar.Builder(
-
                 new NetHttpTransport(),
-
                 JacksonFactory.getDefaultInstance(),
-
                 credentials)
-
                 .setApplicationName("Movie Nights")
-
                 .build();
 
-
-
         DateTime now = new DateTime(System.currentTimeMillis());
-
         Events events = null;
-
         try {
-
             events = calendar.events().list("primary")
-
                     .setMaxResults(10)
-
                     .setTimeMin(now)
-
                     .setOrderBy("startTime")
-
                     .setSingleEvents(true)
-
                     .execute();
-
         } catch (IOException e) {
-
             e.printStackTrace();
-
         }
 
 
         List<Event> items = events.getItems();
-
         if (items.isEmpty()) {
-
             System.out.println("No upcoming events found.");
-
         } else {
-
             System.out.println("Upcoming events");
-
             for (Event event : items) {
-
                 DateTime start = event.getStart().getDateTime();
-
                 if (start == null) { // If it's an all-day-event - store the date instead
-
                     start = event.getStart().getDate();
-
                 }
-
                 DateTime end = event.getEnd().getDateTime();
-
                 if (end == null) { // If it's an all-day-event - store the date instead
-
                     end = event.getStart().getDate();
-
                 }
-
                 System.out.printf("%s (%s) -> (%s)\n", event.getSummary(), start, end);
-
             }
-
         }
 
-
-
-
         return "OK";
-
     }
-
-
 }
